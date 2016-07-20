1e1fb154f78a5436adb371d6002d736e7265dd48
Germán Martín <gmag11@gmail.com>
2016-07-21 00:02:58 +0200

    Reconnection control

--- FirmataMaster.ino
+++ FirmataMaster.ino
@@ -22,39 +22,35 @@
 //SoftwareSerial sserial(12,13); // SoftSerial port does not work reliablily
 WiFiClient firmataRemoteBoard;
 
-void setup()
-{
-	//IPAddress ipadd(192,168,4,1);
-
-	//pinMode(14, OUTPUT);
-	//digitalWrite(14, HIGH);
-	Serial.begin(115200);
-	//Serial.swap();
-	//Serial1.begin(115200);
-	Serial.println();
-	//sserial.begin(57600);
-	//sserial.flush();
-	WiFi.mode(WIFI_STA);
-	WiFi.begin("ESP_0D9D00", "12345678");
-	while (WiFi.status()!=WL_CONNECTED)
-	{
-		DBG_PORT.print(".");
-		delay(1000);
+void reconnect() {
+	if (!WiFi.isConnected()) {
+		//firmataRemoteBoard.stop();
+		WiFi.begin("ESP_0D9D00", "12345678");
+		for (int i = 0; i < 20; i++) {
+			delay(1000);
+			if (WiFi.status() == WL_CONNECTED)
+				break;
+			DBG_PORT.print(".");
+		}
+		DBG_PORT.println();
+		DBG_PORT.printf("IP address: %s\r\n", WiFi.localIP().toString().c_str());
 	}
-	DBG_PORT.println();
-	DBG_PORT.printf("IP address: %s\r\n", WiFi.localIP().toString().c_str());
-	if (!firmataRemoteBoard.connect("192.168.4.1", 23)) {
-		DBG_PORT.println("connection failed");
-		return;
+	if (WiFi.isConnected()) {
+		if (!firmataRemoteBoard.connected()) {
+			DBG_PORT.printf("Connection status 1: %d\r\n", firmataRemoteBoard.status());
+			DBG_PORT.println("Connecting board ...");
+			if (!firmataRemoteBoard.connect("192.168.4.1", 23)) {
+				DBG_PORT.println("connection failed");
+				return;
+			}
+			else
+				DBG_PORT.printf("Connection status 2: %d\r\n", firmataRemoteBoard.status());
+			delay(200);
+		}
 	}
-	/*while (!firmataRemoteBoard.connected())
-	{
-		DBG_PORT.print("+");
-		delay(1000);
-	}*/
-	DBG_PORT.printf("Connection status: %d\r\n",firmataRemoteBoard.status());
-	delay(2000);
+}
 
+void initFirmata() {
 	FirmataClient.begin(firmataRemoteBoard); // Start Firmata communication
 
 	FirmataClient.setSamplingInterval(1000);
@@ -75,43 +71,60 @@ void setup()
 #endif // DEBUG_CAPABILITIES
 }
 
+void setup()
+{
+	Serial.begin(115200);
+	Serial.println();
+	WiFi.mode(WIFI_STA);
+	reconnect();
+	if (WiFi.isConnected())
+		initFirmata();
+}
+
 void loop()
 {
 	// Flash LED every 2 seconds
+	if (WiFi.status() == WL_CONNECTED) {
 #ifdef FIRMATA_DIGITAL_OUTPUT_SUPPORT
 #ifdef DIGITAL_OUTPUT_TEST
-	FirmataClient.digitalWrite(13, HIGH);
-	delay(2000);
-	FirmataClient.digitalWrite(13, LOW);
-	delay(2000);
+		FirmataClient.digitalWrite(13, HIGH);
+		delay(1000);
+		FirmataClient.digitalWrite(13, LOW);
+		delay(1000);
 #endif // DIGITAL_OUTPUT_TEST
 #endif // FIRMATA_DIGITAL_OUTPUT_SUPPORT
 
 #ifdef FIRMATA_ANALOG_INPUT_SUPPORT
 #ifdef ANALOG_INPUT_TEST
-	float voltage0 = (float)FirmataClient.analogRead(0) * (5.0 / 1023);
-	float voltage1 = (float)FirmataClient.analogRead(1) * (5.0 / 1023);
-	float voltage2 = (float)FirmataClient.analogRead(2) * (5.0 / 1023);
-	DBG_PORT.printf("A0: %4.3f V\r\n", voltage0);
-	DBG_PORT.printf("A1: %4.3f V\r\n", voltage1);
-	DBG_PORT.printf("A2: %4.3f V\r\n", voltage2);
+		float voltage0 = (float)FirmataClient.analogRead(0) * (5.0 / 1023);
+		float voltage1 = (float)FirmataClient.analogRead(1) * (5.0 / 1023);
+		float voltage2 = (float)FirmataClient.analogRead(2) * (5.0 / 1023);
+		DBG_PORT.printf("A0: %4.3f V\r\n", voltage0);
+		DBG_PORT.printf("A1: %4.3f V\r\n", voltage1);
+		DBG_PORT.printf("A2: %4.3f V\r\n", voltage2);
 #endif // ANALOG_INPUT_TEST
 #endif // FIRMATA_ANALOG_INPUT_SUPPORT
 
 #ifdef FIRMATA_PWM_OUTPUT_SUPPORT
 #ifdef PWM_OUTPUT_TEST
-	// Fade in LED
-	for (int i = 0; i < 255; i++) {
-		FirmataClient.analogWrite(3, i);
-		delay(5);
-	}
-	// Fade out LED
-	for (int i = 254; i >= 0; i--) {
-		FirmataClient.analogWrite(3, i);
-		delay(5);
-	}
+		// Fade in LED
+		for (int i = 0; i < 255; i++) {
+			FirmataClient.analogWrite(3, i);
+			delay(5);
+		}
+		// Fade out LED
+		for (int i = 254; i >= 0; i--) {
+			FirmataClient.analogWrite(3, i);
+			delay(5);
+		}
 #endif // PWM_OUTPUT_TEST
 #endif // FIRMATA_PWM_OUTPUT_SUPPORT
+	}
+	if (WiFi.status() != WL_CONNECTED) {
+		reconnect();
+		if (WiFi.status() == WL_CONNECTED)
+			initFirmata();
+	}
 	delay(100);
 
 }
